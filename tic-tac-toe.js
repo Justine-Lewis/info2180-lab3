a16e766d3f34990465c5eebbdf787e5febbfee05
Justine Lewis <justinealew@gmail.com>
2024-10-27 18:44:05 -0400

    Added hover function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,31 @@
 document.addEventListener ("DOMContentLoaded", function(){
     const squares = document.querySelectorAll('#board > div');
+    const gameState = Array(9).fill(null);
+
+    let Player = 'X';
+
     
-    squares.forEach(square => {
+    squares.forEach((square, i) => {
         square.classList.add("square");
+
+        square.addEventListener("mouseenter", function(){
+            square.classList.add("hover");
+        });
+
+        square.addEventListener("mouseleave", function(){
+            square.classList.remove("hover");
+        });
+
+        square.addEventListener("click", function() {
+            if (!gameState[i]){
+                gameState[i]=Player;
+                square.textContent = Player;
+                square.classList.add(Player);
+
+
+                Player=Player=== 'X'?'O':'X';
+            }
+        });
     });
 
 });
