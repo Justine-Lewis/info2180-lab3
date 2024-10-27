322ceb77de4ede956f76fa400a694da62e3b2798
Justine Lewis <justinealew@gmail.com>
2024-10-27 19:05:24 -0400

    Added Winner function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,19 @@ document.addEventListener ("DOMContentLoaded", function(){
     const gameState = Array(9).fill(null);
 
     let Player = 'X';
+    const statusDiv = document.getElementById('status');
+    let gameActive = true; //tracks whether the game is still active
+
+    const winningCombos = [
+        [0,1,2], //Row 1
+        [3,4,5], //Row 2
+        [6,7,8], //Row 3
+        [0,3,6], //Column 1
+        [1,4,7], //Column 2
+        [2,5,8], //Column 3
+        [0,4,8], //Leading Diagonal
+        [2,4,6] //Diagonal
+    ];
 
     
     squares.forEach((square, i) => {
@@ -17,15 +30,28 @@ document.addEventListener ("DOMContentLoaded", function(){
         });
 
         square.addEventListener("click", function() {
-            if (!gameState[i]){
+            if (gameActive && !gameState[i]){
                 gameState[i]=Player;
                 square.textContent = Player;
                 square.classList.add(Player);
+
+                if (findWinner(Player)){
+                    statusDiv.textContent = `Congratulations! ${Player} is the Winner!`;
+                    statusDiv.classList.add('you-won');
+                    gameActive = false;
+                    return;//prevent further moves
+                }
 
 
                 Player=Player=== 'X'?'O':'X';
             }
         });
     });
+
+    function findWinner(Player){
+        return winningCombos.some(combination =>{
+            return combination.every(i=> gameState[i]===Player);
+        });
+    }
 
 });
